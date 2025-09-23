e213d67b63134d716ab62335d9d8828ff248eefa
jevgome <jevgome7@gmail.com>
2025-09-23 13:12:23 -0400

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/uconn-schedule-builder/'
+  base: '/uconn-schedule-builder/',
   plugins: [react()],
 })
